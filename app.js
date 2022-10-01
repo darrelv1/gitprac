9c45f27a0b80b4cdb749b2b2435e45bd81fb69c1
Darrel Valdivieso <valdivieso4@hotmail.com>
2022-10-01 12:38:06 -0400

    new htmlCode variable added to app.js

--- app.js
+++ app.js
@@ -5,6 +5,23 @@ console.log('hello world')
 const body = document.querySelector('body'); 
 const collectionItems = document.querySelectorAll('.card');
 
+const htmlCode = `<html>
+  <body>
+  <h1>Welcome to our store!</h1>
+  <button>This is a button</button>
+  <p> thank you for shopping with us!</p>
+    <p>We have a fine selection of medals.</p>
+<<<<<<< HEAD
+  <p>Please choose a section below.</p>
+
+  <div>
+      <a href="bronze.html">Bronze medals</a>
+    </div>
+=======
+>>>>>>> parent of 6c60b30 (Add bronze metals)
+  </body>
+</html>`
+
 //Print out items in the card collections
 for (ele of collectionItems){
 
