d39e27c95289c7866195ef5188a882268d352943
Darrel Valdivieso <valdivieso4@hotmail.com>
2022-10-01 10:16:26 -0400

    for loop of collections

--- app.js
+++ app.js
@@ -5,3 +5,8 @@ console.log('hello world')
 const body = document.querySelector('body'); 
 const collectionItems = document.querySelectorAll('.card');
 
+//Print out items in the card collections
+for (ele of collectionItems){
+
+	console.log(ele)
+}
